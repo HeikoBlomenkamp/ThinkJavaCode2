3dc7c186078ab6bf765a193d4614ead151c8767c
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-02-09 03:59:54 +0000

    modified:   ch01/Hello3.java (textbook version).

--- ch01/Hello3.java
+++ ch01/Hello3.java
@@ -2,7 +2,5 @@ public class Hello3 {
 
     public static void main(String[] args) {
         System.out.print("Hello!\nHow are you doing?\n");
-        System.out.println("She said \"Hello!\" to me.");
     }
-
 }
