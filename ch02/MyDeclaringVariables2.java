7b04cc011163c4c52a4d0111e315e0acb9256af7
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-03-09 23:29:10 +0000

    new file:   ch02/MyDeclaringVariables2.java (R1D49).

---
+++ ch02/MyDeclaringVariables2.java
@@ -0,0 +1,15 @@
+public class MyDeclaringVariables2 {
+
+	public static void main(String[] args) {
+
+		// In general, you should use names
+		// that indicate what the variables mean:
+
+		String firstName;
+		String lastName;
+		int hour, minute;
+
+
+		System.out.println("Chapter 2.1, Check #3.");
+	}
+}
