246423b6d1c38bc40bf0a21f4ed88eb58b8fc17e
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-03-09 23:26:58 +0000

    new file:   ch02/MyDeclaringVariables1.java (R1D49).

---
+++ ch02/MyDeclaringVariables1.java
@@ -0,0 +1,11 @@
+public class MyDeclaringVariables1 {
+
+	public static void main(String[] args) {
+
+		// To declare an integer variable named `x`,
+		// you simply type this:
+		int x;
+
+		System.out.println("Chapter 2.1, Check #2.");
+	}
+}
