209838a3c9c158a1a431a1ea6e26d01042e81938
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-03-09 23:25:52 +0000

    new file:   ch02/MyDeclaringVariables.java (R1D49).

---
+++ ch02/MyDeclaringVariables.java
@@ -0,0 +1,14 @@
+public class MyDeclaringVariables {
+
+	public static void main(String[] args) {
+
+		// This statement is called a **declaration**,
+		// because it declares that the variable `message`
+		// has the type **String**.
+
+		String message;
+
+
+		System.out.println("Chapter 2.1, Check #1.");
+	}
+}
