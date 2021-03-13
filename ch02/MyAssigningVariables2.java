482794fae55d633b3362053b79f142c49dc9276f
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-03-13 23:43:26 +0000

    new file:   ch02/MyAssigningVariables2.java (R1D53).

---
+++ ch02/MyAssigningVariables2.java
@@ -0,0 +1,15 @@
+public class MyAssigningVariables2 {
+
+	public static void main(String[] args) {
+
+
+		// You can also declare and initialize on the same line:
+
+		String message = "Hello!";
+		int hour = 11;
+		int minute = 59;
+
+
+		System.out.println("Chapter 2.2, Check #3.");
+	}
+}
