e07cd1c68d4728be659d547375d64517087d127b
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-03-13 23:41:33 +0000

    new file:   ch02/MyAssigningVariables.java (R1D53).

---
+++ ch02/MyAssigningVariables.java
@@ -0,0 +1,20 @@
+public class MyAssigningVariables {
+
+	public static void main(String[] args) {
+
+		String message;
+		int hour, minute;
+
+
+		// Now that we have declared some variables, we can use
+		// them to store values.
+		// We do that with an **assignment** statement:
+
+		message = "Hello!";  // give message the value "Hello!"
+		hour = 11;           // assign the value 11 to hour
+		minute = 59;         // set minute to 59
+
+
+		System.out.println("Chapter 2.2, Check #1.");
+	}
+}
