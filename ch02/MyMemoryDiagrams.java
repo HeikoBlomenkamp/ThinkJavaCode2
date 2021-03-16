eeb3083ad0f876d4348480b99cdffb9e93d02d0a
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-03-16 23:37:02 +0000

    new file:   ch02/MyMemoryDiagrams.java (R1D56).

---
+++ ch02/MyMemoryDiagrams.java
@@ -0,0 +1,15 @@
+public class MyMemoryDiagrams {
+
+	public static void main(String[] args) {
+
+		// In Java, an assignment statement can make two variables equal,
+		// but they don't have to stay that way:
+
+		int a = 5;
+		int b = a;     // a and b are now equal
+		a = 3;         // a and b are no longer equal
+
+
+		System.out.println("Chapter 2.3, Check #1.");
+	}
+}
