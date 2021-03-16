eae6611673d826290533c8a7eeb94337f0b09361
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-03-16 23:37:52 +0000

    new file:   ch02/MyPrintingVariables.java (R1D56).

---
+++ ch02/MyPrintingVariables.java
@@ -0,0 +1,14 @@
+public class MyPrintingVariables {
+
+	public static void main(String[] args) {
+
+		// The following statements declare a variable named `firstLine`,
+		// assign it the value `"Hello, again!"`, and display that value:
+
+		String firstLine = "Hello, again!";
+		System.out.println(firstLine);
+
+
+		System.out.println("[Chapter 2.4, Check #1.]");
+	}
+}
