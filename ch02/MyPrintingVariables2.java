8c9aed831ef341f2bb0dbc097a7b2682abab83d3
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-03-16 23:39:52 +0000

    new file:   ch02/MyPrintingVariables2.java (R1D56).

---
+++ ch02/MyPrintingVariables2.java
@@ -0,0 +1,19 @@
+public class MyPrintingVariables2 {
+
+	public static void main(String[] args) {
+
+		// Conveniently, the code for displaying a variable
+		// is the same regardless of its type.
+
+		int hour = 11;
+		int minute = 59;
+		System.out.print("The current time is ");
+		System.out.print(hour);
+		System.out.print(":");
+		System.out.print(minute);
+		System.out.println(".");
+
+
+		System.out.println("[Chapter 2.4, Check #3.]");
+	}
+}
