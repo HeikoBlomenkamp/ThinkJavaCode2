2a5f2b7ba355a22b68072b239638b51aa145ae60
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-03-24 03:31:06 +0000

    new file:   ch02/MyArithmeticOperators1.java (R1D64).

---
+++ ch02/MyArithmeticOperators1.java
@@ -0,0 +1,17 @@
+public class MyArithmeticOperators1 {
+
+	public static void main(String[] args) {
+
+		// The following fragment tries to compute the
+		// fraction of an hour that has elapsed.
+		//
+		// By design, integer division always rounds toward zero.
+
+		int minute = 59;
+		System.out.print("Fraction of the hour that has passed: ");
+		System.out.println(minute / 60);
+
+
+		System.out.println("[Chapter 2.5, Check #2.]");
+	}
+}
