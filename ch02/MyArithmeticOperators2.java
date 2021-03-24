2e95943ac25a331fd6d189e572d8591889de5f56
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-03-24 03:31:55 +0000

    new file:   ch02/MyArithmeticOperators2.java (R1D64).

---
+++ ch02/MyArithmeticOperators2.java
@@ -0,0 +1,17 @@
+public class MyArithmeticOperators2 {
+
+	public static void main(String[] args) {
+
+		// From MyArithmeticOperators1.java:
+		// By design, integer division always rounds toward zero.
+		//
+		// As an alternative, we can calculate a percentage rather than a fraction:
+
+		int minute = 59;
+		System.out.print("Percent of the hour that has passed: ");
+		System.out.println(minute * 100 / 60);
+
+
+		System.out.println("[Chapter 2.5, Check #3.]");
+	}
+}
