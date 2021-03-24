7f8c27cdb190dcefecaa359e18e00185f13251df
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-03-24 03:29:56 +0000

    new file:   ch02/MyArithmeticOperators.java (R1D64).

---
+++ ch02/MyArithmeticOperators.java
@@ -0,0 +1,15 @@
+public class MyArithmeticOperators {
+
+	public static void main(String[] args) {
+
+		// The following program converts a time of day to minutes:
+
+		int hour = 11;
+		int minute = 59;
+		System.out.print("Number of minutes since midnight: ");
+		System.out.println(hour * 60 + minute);
+
+
+		System.out.println("[Chapter 2.5, Check #1.]");
+	}
+}
