bed61ab85924a76aab2e63af62e86339e3d12bd5
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-04-05 18:41:50 +0000

    new file:   ch02/MyFloatingPointNumbers3.java (R1D76).

---
+++ ch02/MyFloatingPointNumbers3.java
@@ -0,0 +1,15 @@
+public class MyFloatingPointNumbers3 {
+
+	public static void main(String[] args) {
+
+		// In many cases Java *automatically* converts from
+		// one type to another:
+
+		double y = 1;  // legal, but bad style
+
+		System.out.println("y = " + y);
+
+
+		System.out.println("[Chapter 2.6, Check #4.]");
+	}
+}
