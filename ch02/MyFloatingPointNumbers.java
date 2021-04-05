726684dea50d5f3a4692f906acc337a148d6433d
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-04-05 18:38:06 +0000

    new file:   ch02/MyFloatingPointNumbers.java (R1D76).

---
+++ ch02/MyFloatingPointNumbers.java
@@ -0,0 +1,16 @@
+public class MyFloatingPointNumbers {
+
+	public static void main(String[] args) {
+
+		// You can create **double** variables and assign values to
+		// them the same way we did for the other types:
+
+		double pi;
+		pi = 3.14159;
+
+		System.out.println("pi = " + pi);
+
+
+		System.out.println("[Chapter 2.6, Check #1.]");
+	}
+}
