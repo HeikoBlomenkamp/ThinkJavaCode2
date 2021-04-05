e74ec3757b977bc147cd556e0e5778399127d51a
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-04-05 18:43:43 +0000

    new file:   ch02/MyFloatingPointNumbers5.java (R1D76).

---
+++ ch02/MyFloatingPointNumbers5.java
@@ -0,0 +1,14 @@
+public class MyFloatingPointNumbers5 {
+
+	public static void main(String[] args) {
+
+
+		// The following sets `y` to `0.333333`, as expected:
+
+		double y = 1.0 / 3.0;  // correct
+
+
+		System.out.printf("y = %.6f\n", y);
+		System.out.println("[Chapter 2.6, Check #6.]");
+	}
+}
