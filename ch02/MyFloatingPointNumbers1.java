96ce8a2b98df97e470c2a2ab982c9211c7a6b44b
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-04-05 18:39:47 +0000

    new file:   ch02/MyFloatingPointNumbers1.java (R1D76).

---
+++ ch02/MyFloatingPointNumbers1.java
@@ -0,0 +1,14 @@
+public class MyFloatingPointNumbers1 {
+
+	public static void main(String[] args) {
+
+		// So we can solve the problem from the previous section:
+
+		double minute = 59.0;
+		System.out.print("Fraction of the hour that has passed: ");
+		System.out.println(minute / 60.0);
+
+
+		System.out.println("[Chapter 2.6, Check #2.]");
+	}
+}
