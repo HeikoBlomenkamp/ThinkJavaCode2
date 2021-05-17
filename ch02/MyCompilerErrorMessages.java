cdf468548ba8a6dbc85ab80b8320321a501271b1
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-17 14:50:55 +0200

    new file:   ch02/MyCompilerErrorMessages.java (R2D18).

---
+++ ch02/MyCompilerErrorMessages.java
@@ -0,0 +1,26 @@
+public class MyCompilerErrorMessages {
+
+	public static void main(String[] args) {
+		// generate some simple output
+		// System.out.println("Hello, World!")
+
+		// If you forget the semicolon at the end of the print statement,
+		// you might get an error message like this:
+		//
+		// File: Hello.java  [line: 5]
+		// Error: ';' expected
+
+
+
+		// ~~~~My error message (if executed):
+		//
+		// MyCompilerErrorMessages.java:5: error: ';' expected
+		//                 System.out.println("Hello, World!")
+		//                                                    ^
+		// 1 error
+
+
+
+		System.out.println("\nChapter 2.9, Check #1.");
+	}
+}
