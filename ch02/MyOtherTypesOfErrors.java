73d7fc0d51956dd959e6951491497292e482d77b
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-22 01:02:10 +0200

    new file:   ch02/MyOtherTypesOfErrors.java (R2D22).

---
+++ ch02/MyOtherTypesOfErrors.java
@@ -0,0 +1,23 @@
+public class MyOtherTypesOfErrors {
+
+	public static void main(String[] args) {
+
+		// When a run-time error occurs, the program "crashes" (terminates)
+		// and displays an error message that explains what happened and where.
+		// For example, if you accidentally divide by zero, you will get a
+		// message like this:
+		//
+		// Exception in thread "main" java.lang.ArithmeticException: / by zero
+		//     at Hello.main(Hello.java:5)
+
+		// System.out.println(42 / 0);
+
+
+		// ~~~~My run-time error message (if executed):
+		//
+		// Exception in thread "main" java.lang.ArithmeticException: / by zero
+		//         at MyOtherTypesOfErrors.main(MyOtherTypesOfErrors.java:13)
+
+		System.out.println("[Chapter 2.10, Check #1.]");
+	}
+}
